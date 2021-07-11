eeb6251f32d6101df879b55bd5c10fcadb5ce31c
Larry Tian <Larry98.Tian@gmail.com>
2021-07-10 18:00:46 -0700

    ToDos

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component,useState, useRef, useEffect } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
+import ToDoList from './components/ToDoList'
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+export default function App() {
+    const todoNameRef = useRef();
+    const [todos, setTodos] = useState([{ name: 'read asp.net', complete: false }]);
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+    function handleToDoAdd(e) {
+        const name = todoNameRef.current.value;
+        if (name === '') return
+        console.log(name);
+        setTodos(prevTodos => { return [...prevTodos, { name: name, complete: false }] })
+        todoNameRef.current.value = '';
+    }
+    function toggleTodo(name) {
+        const newTodos = [...todos];
+        const todo = newTodos.find(todo => todo.name === name);
+        todo.complete = !todo.complete;
+        setTodos(newTodos);
+    }
+    /*useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem("todos"));
+        if (storedTodos) setTodos(storedTodos);
+    }, []);
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+    */
+      return (
+          <>
+              <ToDoList todos={todos} toggleTodo={toggleTodo} />
+              <input ref={todoNameRef} />
+              <button onClick={handleToDoAdd} > Add ToDo</button>
+              <button>Clear completed ToDos</button>
+              <div>
+                  0 lefted to dos 
+              </div>
+          </>
+
+
     );
   }
-}
+
--- ClientApp/src/components/ToDoList.js
+++ ClientApp/src/components/ToDoList.js
@@ -1,1 +1,8 @@
-﻿
+﻿import React, { Component } from 'react';
+import ToDo from './ToDo';
+
+export default function ToDoList({ todos, toggleTodo }) {
+    return (
+        todos.map(todo => { return <ToDo todo={todo} toggleTodo={toggleTodo} key={todo.name} /> })
+    )
+}
